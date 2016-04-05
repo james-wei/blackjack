3bd45df3b677ccbe932f765927cdb2da458f7cdc
James Wei <james.wei@berkeley.edu>
2016-04-05 16:00:35 -0700

    override equals and hashcode for Card

--- src/main/java/edu/berkeley/aep/deck/Card.java
+++ src/main/java/edu/berkeley/aep/deck/Card.java
@@ -14,6 +14,21 @@ public class Card {
         this.rank = r;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Card)) return false;
+        Card c = (Card) obj;
+        return this.suite.equals(c.suite) && this.rank.equals(c.rank);
+    }
+
+    @Override
+    public int hashCode() {
+        long v = Double.doubleToLongBits(17 * suite.hashCode() * suite.hashCode() + 51 * rank.hashCode());
+        return (int)(v^(v>>>32));
+    }
+
+    @Override
     public String toString() {
         return rank.toString() + " of " + suite.toString();
     }
--- src/test/java/edu/berkeley/aep/deck/CardTest.java
+++ src/test/java/edu/berkeley/aep/deck/CardTest.java
@@ -3,6 +3,8 @@ package edu.berkeley.aep.deck;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jwei on 4/5/16.
@@ -12,5 +14,21 @@ public class CardTest {
     public void strReprOfTwoOfClubsShouldBeCorrect() {
         Card c = new Card(Rank.TWO, Suite.CLUBS);
         assertEquals("two of clubs", c.toString());
+    }
+
+    @Test
+    public void aceOfSpadesShouldEqualAceOfSpades() {
+        Card c1 = new Card(Rank.ACE, Suite.SPADES);
+        Card c2 = new Card(Rank.ACE, Suite.SPADES);
+        assertTrue(c1.equals(c2));
+        assertTrue(c2.equals(c1));
+    }
+
+    @Test
+    public void threeOfDiamondsShouldNotEqualThreeOfHearts() {
+        Card c1 = new Card(Rank.THREE, Suite.DIAMONDS);
+        Card c2 = new Card(Rank.THREE, Suite.HEARTS);
+        assertFalse(c1.equals(c2));
+        assertFalse(c2.equals(c1));
     }
 }
